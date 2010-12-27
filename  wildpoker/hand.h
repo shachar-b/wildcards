7a60e19f56434492874844a570f82ee9f6db74ca
blame.me <blame.me@82b006e3-cfb3-7549-8677-63f1bdd51797>
2010-12-27 10:55:34 +0000

    ohhhhhhhhh yeah

---  wildpoker/hand.h
+++  wildpoker/hand.h
@@ -19,19 +19,16 @@ public:
 	void setComunityCard(const Card * card,int location);
 	const Card * getCard(int location);
 	void printHand(int startX,int startY);
-	bool operator<(const Hand *otherHand) const;
-	bool operator>(const Hand * otherHand) const;
-	bool operator==(const Hand * otherHand) const;
+	int HandCmp(const Hand* otherHand,handTypes &winningHandType) const;
 	
 private:
 	const Card * m_playerCards[NUM_OF_CARDS_IN_HAND];
-	const Card * m_comunityCards[NUM_OF_CARDS_IN_COMUNITY];
-	int HandCmp(const Hand* otherHand) const;
-	int checkForFours(const Card * sortedCards[],const Card * others_sortedCards[])const;
+	const Card * m_comunityCards[NUM_OF_CARDS_IN_COMUNITY];	
+	int checkForFours(const Card * sortedCards[],const Card * others_sortedCards[],handTypes &winningHandType)const;
 	int checkForPairs(const Card * sortedCards[],const Card * others_sortedCards[],handTypes & rule)const;
 	int rule8(const Card * sortedCards[],const Card * others_sortedCards[],handTypes & rule)const;
-	int checkForStraightFlushOfFive( const Card * sortedCards[],const Card * others_sortedCards[] )const;
-	int checkForStraightFlushOfFour( const Card * sortedCards[],const Card * others_sortedCards[] )const;
-	int checkForStraightOfFive( const Card * sortedCards[],const Card * others_sortedCards[] )const;
-	int checkForStraightOfFour( const Card * sortedCards[],const Card * others_sortedCards[] )const;	
+	int checkForStraightFlushOfFive( const Card * sortedCards[],const Card * others_sortedCards[] ,handTypes &winningHandType)const;
+	int checkForStraightFlushOfFour( const Card * sortedCards[],const Card * others_sortedCards[] ,handTypes &winningHandType)const;
+	int checkForStraightOfFive( const Card * sortedCards[],const Card * others_sortedCards[] ,handTypes &winningHandType)const;
+	int checkForStraightOfFour( const Card * sortedCards[],const Card * others_sortedCards[],handTypes &winningHandType )const;	
 };
